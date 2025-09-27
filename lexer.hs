b3690a6b1341b2de33dbf7d0b33e4a67a088436b
jeangondorek <jeangondorek73@gmail.com>
2025-09-26 21:35:46 -0300

    Create lexer.hs

---
+++ lexer.hs
@@ -0,0 +1,36 @@
+module Lexer where
+import Data.Char (isSpace, isDigit, isAlpha)
+
+data Token = TokenNum Int
+           | TokenPlus
+           | TokenTrue
+           | TokenFalse
+           | TokenAnd
+           | TokenOr
+           | TokenTimes
+           | TokenLParen
+           | TokenRParen
+           deriving Show
+
+lexer :: String -> [Token]
+lexer [] = []
+lexer ('+':cs) = TokenPlus : lexer cs
+lexer ('*':cs) = TokenTimes : lexer cs
+lexer ('(':cs) = TokenLParen : lexer cs
+lexer (')':cs) = TokenRParen : lexer cs
+lexer ('&':'&':cs) = TokenAnd : lexer cs
+lexer ('|':'|':cs) = TokenOr : lexer cs
+lexer (c:cs) | isSpace c = lexer cs
+             | isDigit c = lexNum (c:cs)
+             | isAlpha c = lexKw (c:cs)
+lexer _ = error "Lexical error"
+
+lexNum cs = case span isDigit cs of
+                (num, rest) -> TokenNum (read num) : lexer rest
+
+
+lexKw cs = case span isAlpha cs of
+            ("true", rest) -> TokenTrue : lexer rest
+            ("false", rest) -> TokenFalse : lexer rest
+
+                
